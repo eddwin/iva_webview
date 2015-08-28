4e215619c0ab8d8e31e7d6217a38402a4873d81e
Edwin Mejia <eddwin@live.com>
2015-08-28 11:55:31 -0600

    changes

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'sinatra-contrib'
 gem 'json'
 gem 'tilt'
 gem 'httparty'
+gem 'sinatra-flash'
+gem 'haml'
 
 group :test do
 	gem 'minitest'
--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 require 'json'
 require 'httparty'
+require 'sinatra/flash'
 
 class IvaWebView < Sinatra::Base
+  enable :sessions
+    register Sinatra::Flash
+    use Rack::MethodOverride
 
+    configure :production, :development do
+      enable :logging
+    end
 
   get '/' do
     "Hello There :)"
   end
 
   get '/mostrar_fechas' do
+
+
     dates = get_dates
     '<ul>
       <li>' + "Ultimo dia de IVA es: #{dates[0]} </li>
